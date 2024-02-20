f2dfde0f6c2c95873d2869e8848f24c8f852dbc2
AliGalip75 <galipyetis1111@gmail.com>
2024-02-20 21:56:35 +0300

    First Commit

--- src/pyramid.java
+++ src/pyramid.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class pyramid {
+import java.util.Scanner;
+
+public class pyramid {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i,j,k,n;
+        System.out.println("Piramidin uzunluğunu gir:");
+        n = scan.nextInt();
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= (n-i); j++) {
+                System.out.print(" ");
+            }
+            for (k = 1;k <= (2*i-1);k++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
 }
